ce2a16c338277762ca132af257cfc57fd1665aa1
WangzyaaaA <482673302@qq.com>
2020-10-07 10:55:11 +0800

    jiecheng 1

---
+++ CS08_阶乘/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CS08_阶乘
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int nPos = 1;
+            int[] Array = new int[200];
+            int nCarry = 0;
+            int nSum = 0;
+        }
+    }
+}
