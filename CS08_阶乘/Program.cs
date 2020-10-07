21d30b007dfeb4e06a4c35f46322a857da3c326c
WangzyaaaA <482673302@qq.com>
2020-10-07 11:59:07 +0800

    chachong

--- CS08_阶乘/Program.cs
+++ CS08_阶乘/Program.cs
@@ -14,6 +14,25 @@ namespace CS08_阶乘
             int[] Array = new int[200];
             int nCarry = 0;
             int nSum = 0;
+            Array[0] = 1;
+            int nNum = 20;
+            for(int index = 2;index <= nNum; index++)
+            {
+                Fact(Array, index, ref nPos);
+            }
+        }
+        static void Fact(int[] a,int nNum,ref int nPos)
+        {
+            int nCarry = 0;
+            int nSum = 0;
+            for (int index = 0 ; index < nPos; index++)
+            {
+                
+                nSum = a[index] * nNum;
+                a[index] = nSum % 10;
+                nCarry = nSum / 10;
+
+            }
         }
     }
 }
---
+++ CS09_数组去重/Program.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CS09_数组去重
+{
+    class Program
+    {
+        static void ArrayInit(int[] Array)
+        {
+            Random rd = new Random();
+            for(int index = 0; index < Array.Length; index++)
+            {
+                Array[index] = rd.Next(0, 30);
+            }
+        }
+        static void ArrayRemoval(int[] Array,int n)
+        {
+            int[] Array_b = new int[Array.Length];
+            int nCount = 0;
+            int nArray;
+            for(int aIndex = 0; aIndex < Array.Length; aIndex++)
+            {
+                int temp = 0;
+                for(temp = 0; temp < aIndex; temp++)
+                {
+                    if(Array[aIndex] == Array_b[nCount])
+                    {
+                        break;
+                    }
+                }
+                if(nCount == Array_b.Length)
+                {
+
+                }
+                temp = Array[aIndex];
+                
+            }
+        }
+        static void Main(string[] args)
+        {
+            int[] Array_a = new int[10];
+            
+            ArrayInit(Array_a);
+            
+        }
+    }
+}
