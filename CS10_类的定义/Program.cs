77d316a4d15a4e0ff5645639cf4b4b8aaf1e6f9f
WangzyaaaA <482673302@qq.com>
2020-10-21 11:09:25 +0800

    省长

--- CS10_类的定义/Program.cs
+++ CS10_类的定义/Program.cs
@@ -6,10 +6,37 @@ using System.Threading.Tasks;
 
 namespace CS10_类的定义
 {
+    /// <summary>
+    /// 人员信息
+    /// </summary>
+    class Person
+    {
+        private string cName;
+        private int nAge;
+        public void Say()
+        {
+            Console.WriteLine("姓名：{0}\t年龄：{1}",cName,nAge);
+        }
+        public Person(string cName, int nAge)
+        {
+            this.cName = cName;
+            this.nAge = nAge;
+        }
+        public Person()
+        {
+            cName = "邵市长";
+            nAge = 18;
+        }
+    }
     class Program
     {
         static void Main(string[] args)
         {
+            Person per1 = new Person();
+            per1.Say();
+            Person per2 = new Person("邵省长",19);
+            per2.Say();
+            //邵敏彳亍！
         }
     }
 }
