15fe13324e1d86059b093624aa8516fa30d2ba97
WangzyaaaA <482673302@qq.com>
2020-11-04 10:45:35 +0800

    1

---
+++ CS13_静态关键字/Person.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CS13_静态修饰符
+{
+    class Person
+    {
+        string cName;
+        int nAge;
+    }
+}
---
+++ CS13_静态关键字/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CS13_静态修饰符
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+    }
+}
